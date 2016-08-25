5ee10cf7c961ae74eb8083e6650893baa099033e
jimCresswell <jim.cresswell@gmail.com>
2016-08-25 17:26:24 +0100

    Simplify

--- corr.R
+++ corr.R
@@ -10,22 +10,17 @@ corr <- function(directory, threshold = 0) {
   ## Return a numeric vector of correlations
   
   # Get data from files.
-  pollutant.file.names <- list.files(directory, pattern="*.csv")
-  
-  pollutant.file.paths <- file.path(directory, pollutant.file.names, fsep = .Platform$file.sep)
-  # pollutant.file.paths <- file.path(directory, "001.csv", fsep = .Platform$file.sep)
-  
-  pollutant.data.list <- lapply(pollutant.file.paths, read.csv)
-  
-  # Filter data for a threshold of complete cases.
-  # Note, zero complete cases can be allowed.
-  pollutant.data.list.complete = Filter(function(measurements) {
+  paths <- file.path(directory, list.files(directory, pattern="*.csv"), fsep = .Platform$file.sep)
+
+  output <- vector(mode="numeric")
+
+  for (path in paths) {
+    measurements = read.csv(path)
     count <- length(which(complete.cases(measurements)))
-    count >= threshold
-  }, pollutant.data.list)
-  
-  # Calculate the correlation.
-  Reduce(function(prev, curr) {
-    c(prev, cor(curr$sulfate, curr$nitrate, use = "na.or.complete"))
-  }, pollutant.data.list.complete, vector(mode="numeric"))
+    if (count >= threshold) {
+      output <- c(output, cor(measurements$sulfate, measurements$nitrate, use = "na.or.complete"))
+    }
+  }
+
+  output
 }
